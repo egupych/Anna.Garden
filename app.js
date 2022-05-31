8b36644c3cdba4275066483afe12f67b2eb1207f
Dmitry Egupov <96996225+Egupych@users.noreply.github.com>
2022-05-31 23:13:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -161,6 +161,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+//::::::::::::::::::::::::::::::::::::::::::::::::::
+// Таймер
+//::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
 (function () {
@@ -204,3 +207,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }, 0)
   }());
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::
+// Текст вокруг курсора
+//::::::::::::::::::::::::::::::::::::::::::::::::::
+
+const text = document.querySelector('.text')
+text.innerHTML = text.textContent.replace (/\S/g, "<span>$&</span>");
+
+const element = document.querySelectorAll("span");
+for (let i=0; i < element.length; i++) {
+	element[i].style.transform = "rotate("+i*12+"deg)"
+}
+
+document.addEventListener("mousemove", function(e)
+{
+	text.style.left = e.pageX + "px";
+	text.style.top = e.pageY + "px";
+})
