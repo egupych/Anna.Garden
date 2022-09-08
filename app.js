589822b0780a88f6d555cf3e49efda7237d472c1
Dmitry Egupov <egupovds@gmail.com>
2022-09-08 20:15:46 +0600

    пам
    вау коммит с помощью приложения
    
    экранировал анимацию курсора
    
    Revert "коммит"
    
    This reverts commit 1eca9219d25592128b4be0f1309b775c27006482.

--- app.js
+++ app.js
@@ -212,16 +212,17 @@ document.addEventListener('DOMContentLoaded', () => {
 // Текст вокруг курсора
 //::::::::::::::::::::::::::::::::::::::::::::::::::
 
-const text = document.querySelector('.text')
-text.innerHTML = text.textContent.replace (/\S/g, "<span>$&</span>");
+// const text = document.querySelector('.text')
+// text.innerHTML = text.textContent.replace (/\S/g, "<span>$&</span>");
 
-const element = document.querySelectorAll("span");
-for (let i=0; i < element.length; i++) {
-	element[i].style.transform = "rotate("+i*12+"deg)"
-}
+// const element = document.querySelectorAll("span");
+// for (let i=0; i < element.length; i++) {
+// 	element[i].style.transform = "rotate("+i*12+"deg)"
+// }
 
-document.addEventListener("mousemove", function(e)
-{
-	text.style.left = e.pageX + "px";
-	text.style.top = e.pageY + "px";
-})
+// document.addEventListener("mousemove", function(e)
+// {
+// 	text.style.left = e.pageX + "px";
+// 	text.style.top = e.pageY + "px";
+// })
+
